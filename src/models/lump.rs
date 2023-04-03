e5255d8dbbb42679601e8b684584cc88bff2918e
Théo Bori <theo1.bori@epitech.eu>
2023-04-03 16:49:18 +0200

    [DELETE] Removed useless comments

--- src/models/lump.rs
+++ src/models/lump.rs
@@ -10,26 +10,3 @@ pub trait Lump: Display {
     /// Get the lump data
     fn data(&self) -> LumpData;
 }
-
-// macro_rules! save_raw {
-//     () => {
-//         fn save_raw(&self, dir: &str) {
-//             let path = format!(
-//                 "{}/{}.raw",
-//                 dir,
-//                 self.info.name_ascii()
-//             );
-
-//             let pos = self.info.pos as usize;
-//             let size = self.info.size as usize;
-            
-//             std::fs::write(
-//                 path,
-//                 &self.raw.borrow_mut()[pos..pos + size]
-//             ).unwrap_or_default();
-//         }
-//     };
-// }
-
-// pub use save_raw;
-
